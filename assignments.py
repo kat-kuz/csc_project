bb49664e5d74ac4a12a2a52ab70ffb4dbcb63b01
kat-kuz <kuzina.katja@gmail.com>
2020-11-04 18:44:01 +0300

    update ys algorithm and add results for n=3 and n=4

--- assignments.py
+++ assignments.py
@@ -5,14 +5,24 @@ import itertools
 def calc_welfare(profile, result, n):
     welfare = 0
     for i in range(n):
-        welfare += n - 1 - profile[i].index(result[i])
+        welfare += n - profile[i].index(result[i])
     return welfare
 
 
-def get_yankee_swap_result(profile, n):
-    res = [0]*n
-    held = [set() for _ in range(n)]
+def calc_egal_welfare(profile, result, n):
+    welfare = n
     for i in range(n):
+        welfare = min(welfare, n - profile[i].index(result[i]))
+    return welfare
+
+
+def get_yankee_swap_result(profile, n, egal=False):
+    res = [0] * n
+    for i in range(n):
+        held = [set() for _ in range(n)]
+        for j in range(n):
+            if res[j] != 0:
+                held[j].add(res[j])
         item = profile[i][0]
         curr_agent = i
         while item in res:
@@ -25,34 +35,40 @@ def get_yankee_swap_result(profile, n):
                     item = pref
                     break
         res[curr_agent] = item
-        held[curr_agent].add(item)
-    return calc_welfare(profile, res, n), res
+    if egal:
+        return calc_egal_welfare(profile, res, n), res
+    else:
+        return calc_welfare(profile, res, n), res
 
 
-def get_best_welfare_result(profile, n):
+def get_best_welfare_result(profile, n, egal=False):
     max_welfare = 0
     best_result = [i + 1 for i in range(n)]
     for res in itertools.permutations([i+1 for i in range(n)]):
         if calc_welfare(profile, res, n) > max_welfare:
             max_welfare = calc_welfare(profile, res, n)
             best_result = res
-    return max_welfare, list(best_result)
+    if egal:
+        return calc_egal_welfare(profile, best_result, n), list(best_result)
+    else:
+        return max_welfare, list(best_result)
 
 
-def iterate_over_profiles(n):
+def iterate_over_profiles(n, egal=False):
     ys = 0
     best = 0
-    ys_strategic = 0
-    best_strategic = 0
     for i, profile in enumerate(itertools.product(itertools.permutations([i + 1 for i in range(n)]), repeat=n)):
-        # print(profile)
-        for agent in range(n):
-            ys += get_yankee_swap_result(profile, n)[0]
-            best += get_best_welfare_result(profile, n)[0]
-            ys_strategic += strategic_behavior(profile, n, manipulator=agent)[0]
-            best_strategic += strategic_behavior(profile, n, ys=False, manipulator=agent)[0]
-    return "Yankee Swap: " + str(ys), "Max Welfare: " + str(best), "Yankee Swap with strategy: " + str(ys_strategic), \
-           "Max Welfare with strategy: " + str(best_strategic)
+        best_result = get_best_welfare_result(profile, n, egal)
+        ys_result = get_yankee_swap_result(profile, n, egal)
+        with open('best_result_3.txt', 'a') as f:
+            f.write('\n'.join(map(str, list(profile))) + '\n')
+            f.write(str(best_result[1]) + ' ' + str(best_result[0]) + '\n\n')
+        with open('ys_result_3.txt', 'a') as f:
+            f.write('\n'.join(map(str, list(profile))) + '\n')
+            f.write(str(ys_result[1]) + ' ' + str(ys_result[0]) + '\n\n')
+        best += best_result[0]
+        ys += ys_result[0]
+    return ys, best
 
 
 def strategic_behavior(profile, n, ys=True, manipulator=0):
@@ -102,14 +118,7 @@ def strategic_behavior_best(profile, n, ys=True, manipulator=0):
 
 
 def main():
-    profile = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
-    print(get_yankee_swap_result(profile, 3))
-    print(get_best_welfare_result(profile, 3))
-    print(strategic_behavior(profile, 3, manipulator=0))
-    print(strategic_behavior(profile, 3, ys=False, manipulator=0))
-    # print(strategic_behavior_best(profile, 3, manipulator=0))
-    # print(strategic_behavior_best(profile, 3, ys=False, manipulator=0))
-    # print(iterate_over_profiles(3))
+    print(iterate_over_profiles(3))
 
 
 if __name__ == "__main__":
